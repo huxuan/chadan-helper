1d95bc5320d45c726dc090bd92323100eb228778
huxuan <i@huxuan.org>
2019-08-27 12:37:09 +0800

    Better Notification.

--- notification.py
+++ notification.py
@@ -12,16 +12,16 @@ import requests
 
 SC_URL = 'https://sc.ftqq.com/{}.send?text={}&desp={}'
 TITLE_CONFIRM_ORDER_FORMAT = '[HF]报单{mobile}{msg}'
-TITLE_GET_ORDER_FORMAT = '[HF]抢单{mobile}{operator}{value}元'
+TITLE_GET_ORDER_FORMAT = '[HF]抢单 {mobile} {operator} {value}元'
 CONTENT_ORDER_FORMAT = """
 ## 充值信息
-- 充值号码：**{mobile}**
-- 归属地：**{operator}**
-- 面值：**{value}**
+- 充值号码： **{mobile}**
+- 归属地： **{operator}**
+- 面值： **{value}**
 
 ## 话费单来源
-- 平台：{platform}
-- 账户（*非充值号码！*）：{username}
+- 平台： {platform}
+- 账户（*非充值号码！*）： {username}
 """
 
 
